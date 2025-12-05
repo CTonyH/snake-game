20bf0f8610c716a801d4d87880104561a7f7eebd
Tony H. <tony.hirschligau@googlemail.com>
2025-12-05 12:10:56 +0100

    included simple controls

--- snake.py
+++ snake.py
@@ -6,14 +6,14 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -22,3 +22,15 @@ class Snake:
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(180)
+
+    def left(self):
+        self.head.setheading(270)
+
+    def right(self):
+        self.head.setheading(0)
