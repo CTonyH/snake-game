15eea7e89b01bcf5890ddf1a71c6161f474bd8a8
Tony H. <tony.hirschligau@googlemail.com>
2025-12-05 10:33:02 +0100

    import time for update screen

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from turtle import Screen, Turtle
-
+import time
 from PIL.ImageOps import posterize
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 segment_positions = ((0,0), (-20, 0), (-40, 0))
-
+segments = []
 
 
 for position in segment_positions:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.goto(position)
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+is_on = True
+while is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
+
+
 
 
 
