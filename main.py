36df488d470538a1d71118f925b0e06dcc5ead82
Tony H. <tony.hirschligau@googlemail.com>
2025-12-05 11:27:16 +0100

    refactoring snake elements in snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 from PIL.ImageOps import posterize
 
@@ -7,49 +8,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-segment_positions = ((0,0), (-20, 0), (-40, 0))
-segments = []
 
-
-for position in segment_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 is_on = True
 while is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
