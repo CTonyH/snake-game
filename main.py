0e65cd94ca390c96e08f478496d4e21e2aff36b6
Tony H. <tony.hirschligau@googlemail.com>
2025-12-08 12:11:47 +0100

    added scoreboard to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 import time
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -28,5 +30,7 @@ while is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.add_score()
+
 
 screen.exitonclick()
